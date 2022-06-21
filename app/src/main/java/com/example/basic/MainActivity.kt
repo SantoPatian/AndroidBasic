6a629b30d8888dd3ca06b678c1e8fa6ae8cf9fe4
patian <santo.gitlab@gmail.com>
2022-06-21 21:07:37 +0700

    apply viewBinding to main activity

--- app/src/main/java/com/example/basic/MainActivity.kt
+++ app/src/main/java/com/example/basic/MainActivity.kt
@@ -4,17 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import com.example.basic.databinding.ActivityMainBinding
 
 const val EXTRA_MESSAGE = ""
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        button = binding.sendButton
+        button.setOnClickListener {
+            sendMessage()
+        }
     }
 
-    fun sendMessage(view: View) {
+    private fun sendMessage() {
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
