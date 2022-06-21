a8d670a2fed2e858ba1b03b180acc27689caaeec
patian <santo.gitlab@gmail.com>
2022-06-21 21:11:22 +0700

    apply viewBinding to display message activity

--- app/src/main/java/com/example/basic/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/basic/DisplayMessageActivity.kt
@@ -3,15 +3,22 @@ package com.example.basic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.basic.databinding.ActivityDisplayMessageBinding
 
 class DisplayMessageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDisplayMessageBinding
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_display_message)
+        binding = ActivityDisplayMessageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val message = intent.getStringExtra(EXTRA_MESSAGE)
 
-        val textView = findViewById<TextView>(R.id.textView3).apply {
+        textView = binding.textView3
+        textView.apply {
             text = message
         }
     }
