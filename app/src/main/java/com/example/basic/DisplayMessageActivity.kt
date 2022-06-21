88d71a5d50cc6848c1f4ada3ca56ad74c83c346a
patian <santo.gitlab@gmail.com>
2022-06-21 20:48:31 +0700

    add intent receiver and set text in dispaly message activity

--- app/src/main/java/com/example/basic/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/basic/DisplayMessageActivity.kt
@@ -2,10 +2,17 @@ package com.example.basic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textView3).apply {
+            text = message
+        }
     }
 }
