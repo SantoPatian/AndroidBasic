86c3efd03e844bd50fccf44674e88d14b41c1553
patian <santo.gitlab@gmail.com>
2022-06-21 20:30:16 +0700

    make new function sendMessage in Main Activity

--- app/src/main/java/com/example/basic/MainActivity.kt
+++ app/src/main/java/com/example/basic/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.basic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = ""
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
